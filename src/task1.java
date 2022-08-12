514c751ccbd05f0530b6091c9f58ad09b676c317
GeorgiyZhordaniya <zhordaniageorgiy@mail.ru>
2022-08-12 03:03:25 +0300

    Жордания Г.Р

---
+++ src/task1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите колличество членов последовательности Фибоначчи: ");
+        int n = in.nextInt();
+        System.out.println();
+
+        int[] fib = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0)
+                fib[i] = 0;
+            else if (i == 1)
+                fib[i] = 1;
+            else
+                fib[i] = fib[i-2] + fib[i-1];
+            System.out.print(fib[i] + " ");
+        }
+    }
+}
+
+
+
+
